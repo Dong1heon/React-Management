661e60a0440eed4cb02385adde9162b72c9fc33d
신동헌 <sinsin2159@naver.com>
2023-09-19 14:57:53 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id':1,
+  'image':'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA3MThfNTAg%2FMDAxNjg5NjI2NDA2MTc0.yuXzbpfMgt2w2bLgGHAvuu-EivmgWX6lEPMUUdgvVngg.Pyrp9GMBaMby8jmIHKY5fpu_xVtfKpSEYfjsxYhkUhog.JPEG.2cat1dog%2FKakaoTalk_Photo_2023-07-18-05-39-49_001.jpeg&type=sc960_832',
+  'name':'신동헌',
+  'birthday':'961215',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':2,
+  'image':'https://source.unsplash.com/random',
+  'name':'홍길동',
+  'birthday':'911215',
+  'gender':'여자',
+  'job':'대학생'
+},
+{
+  'id':3,
+  'image':'https://source.unsplash.com/random',
+  'name':'고양이',
+  'birthday':'152926',
+  'gender':'남자',
+  'job':'대학생'
+}
+]
+class App extends Component {
+  render(){
+    return(
+      <div>
+        {
+          customers.map(c => {
+            return <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
